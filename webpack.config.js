3db6867c64bfcdbf32eb249474cdedc5ce6ec353
JeffreyWu <pa022069@gmail.com>
2021-06-22 23:36:09 +0800

    new version v2.0

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@
 */
 
 const page = [
-    {
-        name: "index",
-        function: ["index", "product"]
-    }, {
-        name: "product",
-        function: ["product"]
-    }
+  {
+    name: "index",
+    function: ["index", "product"]
+  }, {
+    name: "product",
+    function: ["product"]
+  }
 ];
 
 // Constructor
@@ -23,107 +23,107 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const entry = page.reduce(function (o, val) { o[val.name] = `src/setting/${val.name}.js`; return o; }, {});
 
 const createPages = page.map((item) =>
-    new HtmlWebpackPlugin({
-        filename: `${item.name}.html`,
-        template: `./src/${item.name}.html`,
-        inject: 'body',
-        chunks: item.function,
-        minify: {
-            removeComments: true,
-            collapseWhitespace: true
-        }
-    })
+  new HtmlWebpackPlugin({
+    filename: `${item.name}.html`,
+    template: `./src/${item.name}.html`,
+    inject: 'body',
+    chunks: item.function,
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true
+    }
+  })
 );
 
 module.exports = {
-    mode: process.env.NODE_ENV,
-    // context: path.resolve(__dirname, 'src'),
-    entry,
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].bundle.js',
-        publicPath: '',
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        publicPath: '/',
-        open: true,
-        watchContentBase: true,
-        port: 8080,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+  mode: process.env.NODE_ENV,
+  // context: path.resolve(__dirname, 'src'),
+  entry,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js',
+    publicPath: '',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    open: true,
+    watchContentBase: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                outputStyle: 'compressed',
+              },
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'images/[name].[ext]',
             },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../'
-                        }
-                    },
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sassOptions: {
-                                outputStyle: 'compressed',
-                            },
-                        }
-                    }
-                ],
+          },
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/"
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: process.env.NODE_ENV === 'production' ? false : true,
             },
-            {
-                test: /\.(png|jpe?g|gif|svg|webp)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192,
-                            name: 'images/[name].[ext]',
-                        },
-                    },
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].[ext]",
-                            outputPath: "images/"
-                        }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            disable: process.env.NODE_ENV === 'production' ? false : true,
-                        },
-                    },
-                ],
-            },
-        ]
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].min.css"
+    }),
+    ...createPages
+  ],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+      '@setting': path.resolve(__dirname, 'src/setting'),
+      '@js': path.resolve(__dirname, 'src/js'),
+      '@css': path.resolve(__dirname, 'src/css'),
+      '@img': path.resolve(__dirname, 'src/images'),
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "css/[name].min.css"
-        }),
-        ...createPages
-    ],
-    resolve: {
-        alias: {
-            src: path.resolve(__dirname, 'src'),
-            '@setting': path.resolve(__dirname, 'src/setting'),
-            '@js': path.resolve(__dirname, 'src/js'),
-            '@css': path.resolve(__dirname, 'src/css'),
-            '@img': path.resolve(__dirname, 'src/images'),
-        },
-        extensions: ['.js', '.jsx']
-    },
+    extensions: ['.js', '.jsx']
+  },
 };
